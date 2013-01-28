30132379c772ef1be05d0528f7928ad4adb6a697
newnuy <newnuy@newnuy.com>
2013-01-28 16:51:23 +0800

    [com][themes][minimatica] add the wordpress theme named minimatica

---
+++ com/themes/minimatica/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The footer Template
+ *
+ * @package WordPress
+ * @subpackage minimatica
+ * @since Minimatica 1.0
+ */
+ ?>
+ 		<footer id="footer">
+			<?php get_sidebar( 'footer' ); ?>
+ 			<nav id="access" role="navigation">
+ 				<?php wp_nav_menu( array( 'theme_location'  => 'primary_nav', 'container_id' => 'primary-nav', 'container_class' => 'nav', 'fallback_cb' => 'minimatica_nav_menu' ) ); ?>
+			</nav><!-- #access -->
+<p>深圳B-USTC爱乒群 | http://bustc.com | All Rights Reserved.</p>
+		</footer><!-- #footer -->
+	</div><!-- #wrapper -->
+	<?php wp_footer(); ?>
+</body>
+</html>
